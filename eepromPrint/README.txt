d89f1a80f546e0d53750723ee8992e98a317b18c
anonymous-void <anonymous4reg@gmail.com>
2015-11-26 21:30:57 +0800

    eeprom data read from chips, and recorded in eepromReadResult...

---
+++ eepromPrint/README.txt
@@ -0,0 +1,2 @@
+This program read all the chip's EEPROM, and print them through Serial port.
+----2015/11/26
---
+++ eepromPrint/eepromPrint.ino
@@ -0,0 +1,21 @@
+#include <EEPROM.h>
+
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(115200);
+    int address;
+    unsigned int value;
+    for (address=0; address<EEPROM.length(); address++)
+    {
+        value = EEPROM.read(address);
+        Serial.print(value, BIN);
+        Serial.print("\t");
+        if ((address+1)%8 == 0)
+            Serial.println("");
+    }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
